e2c22fe03bb5086fd97300957c54e0bd8b16582a
v-andrade-dev <vini.andrade0@gmail.com>
2023-02-03 16:23:09 -0300

    autenticação de login, com jwtHelper e guard do angular cli

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
 import { TechnicianListComponent } from './components/technician/technician-list/technician-list.component';
 
 const routes: Routes = [
+  {path:'login', component: LoginComponent},
+
   {
-    path: '', component: NavComponent, children: [
+    path: '', component: NavComponent, canActivate:[AuthGuard], children: [
       {path: 'home', component: HomeComponent},
       {path: 'technician', component: TechnicianListComponent}
     ]
-  }, 
-  {
-    path:'login', component: LoginComponent
   }
 ];
 
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Credentials } from 'src/app/models/credentials';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -18,21 +20,21 @@ export class LoginComponent implements OnInit {
   email = new FormControl(null, Validators.email);
   password = new FormControl(null, Validators.minLength(3));
 
-  constructor(private toast: ToastrService) { }
+  constructor(private toast: ToastrService, private service: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   logar(){
-    this.toast.error('Usuário e/ou senha inválidos!', 'Login');
-    this.creds.password = '';
+    this.service.authenticate(this.creds).subscribe(res =>{
+      this.service.successfullLogin(res.headers.get('Authorization').substring(7));
+      this.router.navigate(['']);
+    }, ()=> {
+      this.toast.error('Usuário e/ou senha inválidos!')
+    })
   }
 
   validateFields(): boolean{
-    if(this.email.valid && this.password.valid){
-      return true;
-    }else{
-      return false;
-    }
+   return this.email.valid && this.password.valid;
   }
 }
