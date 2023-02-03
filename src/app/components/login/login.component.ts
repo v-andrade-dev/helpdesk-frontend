8c0010e5ed5067b689e2ded94dba58b57ac1cf67
v-andrade-dev <vini.andrade0@gmail.com>
2023-02-03 10:55:59 -0300

    mensagem de erro com toastr

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { Credentials } from 'src/app/models/credentials';
 
 @Component({
@@ -17,11 +18,15 @@ export class LoginComponent implements OnInit {
   email = new FormControl(null, Validators.email);
   password = new FormControl(null, Validators.minLength(3));
 
-  constructor() { }
+  constructor(private toast: ToastrService) { }
 
   ngOnInit(): void {
   }
 
+  logar(){
+    this.toast.error('Usuário e/ou senha inválidos!', 'Login');
+    this.creds.password = '';
+  }
 
   validateFields(): boolean{
     if(this.email.valid && this.password.valid){
