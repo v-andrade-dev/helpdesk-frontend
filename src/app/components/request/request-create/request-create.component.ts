d88e75dc103ea2844ada97aab0104d7f02efa107
v-andrade-dev <vini.andrade0@gmail.com>
2023-02-20 15:32:21 -0300

    validação de formulario, criação de chamados

--- src/app/components/request/request-create/request-create.component.ts
+++ src/app/components/request/request-create/request-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-request-create',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestCreateComponent implements OnInit {
 
+  title: FormControl = new FormControl(null, [Validators.required])
+  status: FormControl = new FormControl(null, [Validators.required])
+  priority: FormControl = new FormControl(null, [Validators.required])
+  technician: FormControl = new FormControl(null, [Validators.required])
+  client: FormControl = new FormControl(null, [Validators.required])
+  note: FormControl = new FormControl(null, [Validators.required])
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  validateFields():boolean{
+    return this.title.valid && this.status.valid && this.priority.valid &&
+    this.technician.valid && this.client.valid && this.note.valid
+  }
 }
